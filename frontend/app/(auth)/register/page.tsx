e6efa893ecbc4bc8ea7035d51e27f1e80cb70c83
tolgabayrakdev <bayraktolga28@gmail.com>
2023-08-24 14:34:35 +0300

    register page

---
+++ frontend/app/(auth)/register/page.tsx
@@ -0,0 +1,55 @@
+"use client"
+import React from 'react'
+import { Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
+
+type Props = {}
+
+export default function page({ }: Props) {
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
+    return (
+        <div>
+            <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Create account here</h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" action="#" method="POST">
+                        <div>
+                            <Input placeholder='Username' />
+                        </div>
+                        <div>
+                            <Input placeholder='Email' />
+                        </div>
+
+                        <div>
+                            <InputGroup size='md'>
+                                <Input
+                                    pr='4.5rem'
+                                    type={show ? 'text' : 'password'}
+                                    placeholder='Enter password'
+                                />
+                                <InputRightElement width='4.5rem'>
+                                    <Button h='1.75rem' size='sm' onClick={handleClick}>
+                                        {show ? 'Hide' : 'Show'}
+                                    </Button>
+                                </InputRightElement>
+                            </InputGroup>
+                        </div>
+
+                        <div>
+                            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Not a member?
+                        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 ml-1">Start a 14 day free trial</a>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,24 @@
+"use client"
+import Link from 'next/link'
+import { motion } from "framer-motion"
+
 export default function Home() {
   return (
     <main>
-    
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="h-screen flex justify-center items-center flex-col font-mono duration-300">
+          <h1 className='text-2xl mb-3'>Welcome</h1>
+          <div className=''>
+            <Link className=' hover:underline text-orange-500' href="/register">Start here.</Link>
+          </div>
+        </div>
+      </motion.div>
+
+
     </main>
   )
 }
