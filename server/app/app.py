bbe7645d5a25349765bd867936a9deeb9cd944d6
tolgabayrakdev <bayraktolga28@gmail.com>
2023-08-26 19:22:51 +0300

    requiremnts txt file added

--- server/app/app.py
+++ server/app/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
-from database import db
+from flask import Flask, jsonify
+from database import DatabaseHelper
+from sqlalchemy import text
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://root:root@localhost/postgres"
 
-db.init_app(app)
+db_uri = "postgresql://root:root@localhost/postgres"
+db_helper = DatabaseHelper(db_uri)
+
+
+@app.route("/deneme", methods=["GET"])
+def deneme_query():
+    sql_query = "SELECT * FROM users"
+    rows = db_helper.execute_query(text(sql_query))
+    user_list = [{"username": row.username} for row in rows]
+    return jsonify({"users": user_list})
 
 
 if __name__ == "__main__":
--- server/app/database.py
+++ server/app/database.py
@@ -1,3 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
-db = SQLAlchemy()
+class DatabaseHelper:
+    def __init__(self, db_uri):
+        self.engine = create_engine(db_uri)
+    
+    def execute_query(self, sql_query):
+        with self.engine.connect() as connection:
+            result = connection.execute(sql_query)
+            rows = result.fetchall()
+        return rows
