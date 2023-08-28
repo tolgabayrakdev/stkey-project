9724d6e9687f4d758bb3461637e6081d9b4e419e
tolgabayrakdev <bayraktolga28@gmail.com>
2023-08-28 22:16:31 +0300

    tokens added for auth

--- server/app/service/auth_service.py
+++ server/app/service/auth_service.py
@@ -1,17 +1,30 @@
 from sqlalchemy import text
 from werkzeug.exceptions import HTTPException
 from util.helper import Helper
-import os
 from database import connection
+import psycopg2
 
 
 class AuthService:
     @staticmethod
     def login(email: str, password: str):
         hashed_password = Helper.generate_hash_password(password)
-        cur = connection.cursor()
-        cur.execute("SELECT * FROM users WHERE email = %s and password = %s", (email, hashed_password))
-        data = cur.fetchall()
-        print("-------")
-        print(data)
-        return data
+        try:
+            cur = connection.cursor()
+            cur.execute(
+                "SELECT * FROM users WHERE email = %s and password = %s",
+                (email, hashed_password),
+            )
+            data = cur.fetchall()
+            connection.commit()
+            print(data[0][0])
+            if data:
+                access_token = Helper.generate_access_token({"email": data[0][0]})
+                refresh_token = Helper.generate_access_token({"email": data[0][0]})
+                print(access_token)
+                return {"access_token": access_token, "refresh_token": refresh_token}
+            else:
+                raise HTTPException(description="User not found!", response=404)
+        except psycopg2.DatabaseError as error:
+            connection.rollback()
+            raise error
